e86f3743c430dfcb0cd11f66062213bd157ea87e
alprof <alprof@mlabs>
2022-05-28 22:55:44 +0200

    "new file"

--- js_script.js
+++ js_script.js
@@ -1,2 +1,93 @@
-var = 2;
-var = "alprof";
+
+
+
+
+
+
+/*
+const button = document.querySelector('button');
+
+button.addEventListener('click', event => {
+    waitfunction();
+});
+async function waitfunction() {
+setTimeout('alertbox()', 1000);
+}
+function alertbox() {
+    window.alert('10');
+}
+
+function display(word) {
+    document.getElementById("demo").innerHTML;
+}
+
+function worldOne() { display("Go"); }
+function wordTwo() { display("Stop"); }
+function wordThree() { display("Yield"); }
+
+wordThree();
+wordTwo();
+worldOne();
+var i;
+var y = 6;
+var x = ["b", 1, 2, 5];
+for (i = 1; i < x.length; i++){
+    y += x[i];
+}
+console.log(y);
+
+a = [1, 2, 3];
+b=[3];
+c = a * b;
+console.log(c);
+
+var l = 1;
+var m = "sp";
+if (l == true) {
+    m-=1;
+}
+console.log(m);
+
+function displaystring() {
+    console.log("John");
+    setTimeout(function ()   { console.log("Olive"); }, 400);
+    console.log("Mailk")
+}
+displaystring();
+
+
+*/
+var went = true;
+var going = went ? "yes" : "no";
+var x = "yes";
+if (going != "no") {
+    x += 1 + "no";
+}
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
